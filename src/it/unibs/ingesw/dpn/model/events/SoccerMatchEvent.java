b7492a6e1ce59a965184cfa4ae30fd852dad877d
gifisigustra <gifisigustra@gmail.com>
2019-05-21 17:14:00 +0200

    Sistemato errore commesso da Lorenzo nel merge precedente

--- src/it/unibs/ingesw/dpn/model/events/SoccerMatchEvent.java
+++ src/it/unibs/ingesw/dpn/model/events/SoccerMatchEvent.java
@@ -8,7 +8,6 @@ import it.unibs.ingesw.dpn.model.categories.CategoryProvider;
 import it.unibs.ingesw.dpn.model.fields.Field;
 import it.unibs.ingesw.dpn.model.fieldvalues.FieldValue;
 import it.unibs.ingesw.dpn.model.users.User;
-import it.unibs.ingesw.dpn.ui.UIRenderer;
 
 /**
  * Classe che rappresenta concettualmente il tipo di categoria "Partita di calcio".
@@ -35,5 +34,17 @@ public class SoccerMatchEvent extends Event {
 	SoccerMatchEvent(User creator, Map<Field, FieldValue> fieldValues) {
 		super(creator, CategoryEnum.PARTITA_DI_CALCIO, fieldValues);
 	}
-	
+
+	@Override
+	public String toString() {
+		StringBuffer exit = new StringBuffer();;
+		exit.append("Evento : Partita di calcio");
+		Category cat = CategoryProvider.getProvider().getCategory(CategoryEnum.PARTITA_DI_CALCIO);
+		for (Field f : cat.getFields()) {
+			exit.append(String.format(" | %-35s : %s",
+					f.getName(),
+					this.getFieldValue(f).toString()));
+		}
+		return exit.toString();
+	}
 }
