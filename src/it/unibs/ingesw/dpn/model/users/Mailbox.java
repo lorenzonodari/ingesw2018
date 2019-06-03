092d7509dc81c15f3c4ab5f6073096227c4d37e6
Lorenzo Nodari <28101148+lorenzonodari@users.noreply.github.com>
2019-06-03 13:03:55 +0200

    Sistemata documentazione

--- src/it/unibs/ingesw/dpn/model/users/Mailbox.java
+++ src/it/unibs/ingesw/dpn/model/users/Mailbox.java
@@ -17,6 +17,7 @@ public class Mailbox implements Serializable {
 	
 	private LinkedList<Notification> notifications;
 	private LinkedList<Invite> invitations;
+	
 	/**
 	 * Crea una nuova mailbox, vuota
 	 */
@@ -38,12 +39,12 @@ public class Mailbox implements Serializable {
 	}
 	
 	/**
-	 * Aggiunge la notifica/invito data alla mailbox
+	 * Aggiunge la notifica data alla mailbox
 	 * 
 	 * Precondizione: la notifica data non deve gia' essere contenuta nella mailbox
 	 * Postcondizione: la notifica data non e' piu' presente nella mailbox
 	 * 
-	 * @param notification La notifica da aggiungere alla mailbox
+	 * @param toAdd La notifica da aggiungere alla mailbox
 	 */
 	public void deliver(Notification toAdd) {
 		
@@ -57,6 +58,15 @@ public class Mailbox implements Serializable {
 		assert this.notifications.contains(toAdd);
 		
 	}
+	
+	/**
+	 * Aggiunge l'invito dato alla mailbox
+	 * 
+	 * Precondizione: l' invito dato non deve gia' essere contenuta nella mailbox
+	 * Postcondizione: l' invito dato non e' piu' presente nella mailbox
+	 * 
+	 * @param toAdd L'invito da aggiungere alla mailbox
+	 */
 	public void deliver(Invite toAdd) {
 		
 		// Verifica delle precondizioni
@@ -74,8 +84,9 @@ public class Mailbox implements Serializable {
 	 * Rimuove la notifica/invito data dalla mailbox
 	 * 
 	 * Precondizione: la notifica data deve essere contenuta nella mailbox
-	 * Postcondizione: la notifica data 
-	 * @param notification
+	 * Postcondizione: la notifica data non e' piu' contenuta nella mailbox
+	 * 
+	 * @param toDelete La notifica da eliminare dalla mailbox
 	 */
 	public void delete(Notification toDelete) {
 		
@@ -88,6 +99,15 @@ public class Mailbox implements Serializable {
 		
 		assert !this.notifications.contains(toDelete);
 	}
+	
+	/**
+	 * Rimuove l'invito data dalla mailbox
+	 * 
+	 * Precondizione: l'invito dato deve essere contenuto nella mailbox
+	 * Postcondizione: l'invito dato non e' piu' contenuto nella mailbox 
+	 * 
+	 * @param toDelete L'invito da eliminare dalla mailbox
+	 */
 	public void delete(Invite toDelete) {
 		
 		// Verifica delle precondizioni
@@ -101,7 +121,8 @@ public class Mailbox implements Serializable {
 	}
 	
 	/**
-	 * Restituisc true se la mailbox dell'utente e' attualmente vuota
+	 * Restituisc true se la mailbox dell'utente e' attualmente vuota, ovvero
+	 * non contiene nessuna notifica.
 	 * 
 	 * @return true se la mailbox dell'utente e' vuota
 	 */
