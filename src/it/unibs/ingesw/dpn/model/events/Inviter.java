ca221d94f5878271f7e929e7111486d2184d6d02
Lorenzo Nodari <28101148+lorenzonodari@users.noreply.github.com>
2019-06-04 16:36:16 +0200

    Refactoring della classe Inviter

--- src/it/unibs/ingesw/dpn/model/events/Inviter.java
+++ src/it/unibs/ingesw/dpn/model/events/Inviter.java
@@ -1,7 +1,9 @@
 package it.unibs.ingesw.dpn.model.events;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
 
 import it.unibs.ingesw.dpn.model.EventBoard;
 import it.unibs.ingesw.dpn.model.users.Invite;
@@ -10,56 +12,131 @@ import it.unibs.ingesw.dpn.model.users.User;
 public class Inviter {
 	
 	private Event target;
-	private List<User> notInvited;
-	private List<User> invited = new ArrayList<>();
+	private HashMap<User, Boolean> invited = new HashMap<>();
 		
 	public Inviter(Event target, EventBoard board) {
+		
 		this.target = target;
-		notInvited = board.getListOfOldSubscribersFromPastEvents(target.getCreator());
+		for (User u : board.getListOfOldSubscribersFromPastEvents(target.getCreator())) {
+			invited.put(u, false);
+		}
+		
 		
 	}
 	/**
-	 * metodo che prende un utente della lista delle persone da non invitare e 
-	 * lo mette in quella degli utenti da invitare 
-	 * @param invite: utente da invitare
+	 * Imposta l'utente dato come utente da invitare
+	 * 
+	 * Precondizione: l'utente dato deve essere uno dei possibili canditati all'invito. Cio' significa
+	 *                che egli deve aver partecipato in passato ad almeno un evento tra quelli proposti
+	 *                dall'attuale creatore dell'evento, della stessa categoria dell'evento attuale
+	 * 
+	 * @param user L'utente da invitare
 	 * @return false se il processo non è andato a buon fine
 	 */
-	public boolean addInvitation(User invite) {
-		if(!notInvited.contains(invite))
+	public boolean addInvitation(User user) {
+		
+		// Verifica delle precondizioni
+		if (!invited.containsKey(user)) {
 			return false;
-		notInvited.remove(invite);
-		invited.add(invite);
+		}
+		
+		invited.put(user, true);
 		return true;
+		
 	}
+	
 	/**
-	 * Metodo che prende un utente dell lista degli utenti da invitare e lo mette
-	 * nella lista di quelli da non invitare 
-	 * @param toRemove: utente da rimuovere dalla lista degli utenti da invitare
+	 * Imposta l'utente dato come utente da non invitare
+	 * 
+	 * Precondizione: l'utente dato deve essere uno dei possibili candidati all'invito. Cio' significa
+	 *                che egli deve aver partecipato in passato ad almeno un evento tra quelli propost
+	 *                dall'attuale creatre dell'evento, della stessa categoria dell'evento attuale
+	 * 
+	 * @param user L'utente da rimuovere dalla lista degli utenti da invitare
 	 * @return false se il processo non è andato a buon fine
 	 */
-	public boolean removeInvitation(User toRemove) {
-		if(!invited.contains(toRemove))
-				return false;
-		notInvited.add(toRemove);
-		invited.remove(toRemove);
+	public boolean removeInvitation(User user) {
+		
+		// Verifica delle precondizioni
+		if (!invited.containsKey(user)) {
+			return false;
+		}
+				
+		invited.put(user, false);
 		return true;
+		
 	}
+	
+	/**
+	 * Restituisce l'insieme degli utenti candidati all'invito all'evento.
+	 * 
+	 * @return L'insieme dei candidati all'invito
+	 */
+	public Set<User> getCandidates() {
+		
+		return invited.keySet();
+
+	}
+	
 	/**
 	 * Metodo che restituisce la lista degli eventi da invitare
+	 * 
+	 * @return La lista degli utenti selezionati per l'invio dell'invito
 	 */
 	public List<User> getInvited(){
-		return invited;
+		
+		ArrayList<User> retVal = new ArrayList<>();
+		for (User u : invited.keySet()) {
+			if (invited.get(u)) {
+				retVal.add(u);
+			}
+		}
+		return retVal;
+				      
 	}
+	
 	/**
 	 * Metodo che restituisce la lista degli utenti da non invitare
+	 * 
+	 * @return La lista degli utenti non selezionati per l'invio dell'invito
 	 */
 	public List<User> getNotInvited(){
-		return notInvited;
+		
+		ArrayList<User> retVal = new ArrayList<>();
+		for (User u : invited.keySet()) {
+			if (!invited.get(u)) {
+				retVal.add(u);
+			}
+		}
+		return retVal;
 	}
-	//TODO
+	
+	/**
+	 * Restituisce true se l'utente dato e' attualmente selezionato come utente da invitare
+	 * 
+	 * @param u L'utente del quale si desiderano informazioni
+	 * @return true se l'utente dato e' attualmente selezionato per l'invito
+	 */
+	public boolean isInvited(User u) {
+		
+		// Verifica delle precondizioni
+		if (!invited.containsKey(u)) {
+			throw new IllegalArgumentException();
+		}
+		
+		return invited.get(u);
+		
+	}
+	
+	/**
+	 * Invia gli inviti agli utenti selezionati
+	 */
 	public void sendInvites() {
-		for(User p : invited) {
-			p.getMailbox().deliver(new Invite(this.target));
+		for(User p : invited.keySet()) {
+			
+			if (invited.get(p)) {
+				p.getMailbox().deliver(new Invite(this.target));
+			}
 		}
 	}
 }
