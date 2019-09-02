eeaf183191611b457d34db0642a649be8e592b76
Lorenzo Nodari <28101148+lorenzonodari@users.noreply.github.com>
2019-09-02 22:26:19 +0200

    Rimosso import inutilizzato

--- test/it/unibs/ingesw/dpn/model/users/NotificationTest.java
+++ test/it/unibs/ingesw/dpn/model/users/NotificationTest.java
@@ -2,10 +2,7 @@ package it.unibs.ingesw.dpn.model.users;
 
 import static org.junit.Assert.*;
 
-import org.junit.Before;
 import org.junit.Test;
-
-import it.unibs.ingesw.dpn.model.users.Notification;
 
 public class NotificationTest {
 	
